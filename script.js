ad0f5112c248bd75394f87347e9b5ce03adaf45e
AzureDreams <AzureDreams@users.noreply.github.com>
2026-05-13 02:44:27 -0500

    populated users input in list

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let allTasks = [];
 function addTask (){
   let userInput = document.querySelector("#new_task");
   let taskText = userInput.value;
-console.log(taskText);
+if(taskText === "")return;
+allTasks.push(taskText);
+let listItem = document.createElement("li");
+listItem.innerText = taskText;
+
+let taskList = document.querySelector("#user_task");
+taskList.appendChild(listItem);
+userInput.value ="";
 }
+
