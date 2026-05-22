4eb60015970696fb3de778fa18a62766befc9429
AzureDreams <AzureDreams@users.noreply.github.com>
2026-05-22 01:53:35 -0500

    added delete button

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
+//John 3:16
 // Create array to hold all tasks
 let allTasks = [];
-// Create Variable to target user input
+// target user input
 let userInput = document.querySelector("#new_task");
-// Create function to add task when called
+//  add task when called
 function addTask (){
-  //Declare vairbale to store users input
+  //store users input
   let taskText = userInput.value;
-  // If statemant to prevent users from entering empty values
+  // If statement to prevent users from entering empty values
 if(taskText === "")return;
 //Push the task into the array 
 allTasks.push(taskText);
 //Create list item
 let listItem = document.createElement("li");
+// Create a span to customize element later
+let taskSpan = document.createElement("span");
+taskSpan.innerText =taskText;
+
+//Create delete button 
+let deleteBtn =document.createElement("button");
+deleteBtn.innerText= "🗑️";
+deleteBtn.classList.add("delete-btn");
+deleteBtn.onclick = function(){
+  listItem.remove();
+  allTasks = allTasks.filter(task => task !== taskText);
+}
+
 // Assing the text value to the list item
-listItem.innerText = taskText;
+listItem.appendChild(taskSpan);
+listItem.appendChild(deleteBtn);
 // Target the unordered list
 let taskList = document.querySelector("#user_task");
 // Make the task/li visbile on the docuemnt
 taskList.appendChild(listItem);
+
+
 // Clear the searchbox so user can enter more tasks afterwards
 userInput.value ="";
 }
 
+ 
