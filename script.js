3d9940a55ee4288b1d2b6b01749419ee265d4149
AzureDreams <AzureDreams@users.noreply.github.com>
2026-06-15 00:58:33 -0500

    Cleaned up addTask function

--- script.js
+++ script.js
@@ -7,20 +7,17 @@ const userInput = document.querySelector("#new-task");
 const taskList = document.querySelector("#task-list");
 const addBtn = document.querySelector("#add-btn");
 
-// Add task when button is clicked
-addBtn.addEventListener("click", addTask);
+addBtn.addEventListener("click",addTask);
 
 function addTask (){
-  const taskText = userInput.value.trim();
-  if (taskText === "")return; // Prevent user form adding empty task
-  allTasks.push(taskText);
-
   //Create list item
   const listItem = document.createElement("li");
- 
-  //Create span for task test so we can style later
-  const taskSpan = document.createElement("span");
-  taskSpan.textContent = taskText;
+  listItem.textContent = userInput.value;
+  allTasks.push(listItem);
+ taskList.appendChild(listItem);
+
+  
+}
   
   //Create delete button
   const deleteBtn = document.createElement("button");
@@ -33,10 +30,9 @@ function addTask (){
   });
 
   //Append elements so we can see them visually
-  listItem.appendChild(taskSpan);
-  listItem.appendChild(deleteBtn);
-  taskList.appendChild(listItem);
+
+  
 
   //Clear input after each task entered
   userInput.value = "";
-}
+
